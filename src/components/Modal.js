b4cfa1e4028d27307a76a52c66b5c3eceaf883b7
brandonawilliams1 <you@example.com>
2022-03-19 22:57:19 -0500

    changed the homepage url in pkg json

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,33 +2,33 @@ import { NavLink } from 'react-router-dom';
 import './styles/Modal.css'
 import { Close } from '@material-ui/icons';
 
-export default function Modal ({showModal}) {
-  return (
+export default function Modal({ showModal }) {
+    return (
         <div className='modal'>
 
             <div className="close-modal">
-                <Close className='cm' onClick={showModal}/>
-            </div>
-            
-            <div className="cart">
-             <a href="https://www.linkedin.com/in/brandon-williams-kodewerk/">linkedin</a>
+                <Close className='cm' onClick={showModal} />
             </div>
 
             <div className="cart">
-            <a href="https://unpuffd.herokuapp.com/">Group Project</a>
+                <a href="https://www.linkedin.com/in/brandon-williams-kodewerk/">linkedin</a>
             </div>
 
             <div className="cart">
-            <a href="https://github.com/brandonawilliams1/SQL-Employee-Tracker">SQL Employee Tracker CLI</a>
+                <a href="https://unpuffd.herokuapp.com/">Group Project</a>
             </div>
 
             <div className="cart">
-            <a href="https://github.com/brandonawilliams1/SQL-Employee-Tracker">SQL Employee Tracker CLI</a>
+                <a href="https://github.com/brandonawilliams1/SQL-Employee-Tracker">SQL Employee Tracker CLI</a>
+            </div>
+
+            <div className="cart">
+                <a href="https://github.com/brandonawilliams1/SQL-Employee-Tracker">SQL Employee Tracker CLI</a>
             </div>
             <div className="cart">
-            <a href="https://github.com/EMJBB-Project-3">Full Stack MERN App</a>
+                <a href="https://github.com/EMJBB-Project-3">Full Stack MERN App</a>
             </div>
         </div>
-  );
+    );
 };
 
